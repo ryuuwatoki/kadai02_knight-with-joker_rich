d025787c2e2bd192eda81cadc236a141d38d4ecc
ryuuwatoki <ryuuwatoki@gmail.com>
2023-11-03 03:10:11 +0900

    first commit

---
+++ js_cn/main.game.js
@@ -0,0 +1,117 @@
+
+
+var getcard = function() {
+
+
+    var winSound = new Audio('sound/win.mp3')
+    var drawSound = new Audio('sound/draw.mp3')
+    var loseSound = new Audio('sound/lose.mp3')
+    var reverseWinSound = new Audio('sound/reverse_win.mp3')
+
+
+
+
+    var random, random2;
+
+do {
+    random = Math.round(Math.random() * 3 + 1);
+    myImg.src = 'img/poker0'+ random +'.png';
+    random2 = Math.round(Math.random() * 3 + 1);
+    comImg.src = 'img/poker0'+ random2 +'.png'
+} while (random === random2);
+
+
+    if(random === 1 && random2 === 1){
+        boxC.innerHTML = 'DRAW!';
+        drawSound.play()
+
+    }
+    if(random === 1 && random2 === 2){
+        boxC.innerHTML = 'DRAW!';
+        drawSound.play()
+
+    }
+    if(random === 1 && random2 === 3){
+        boxC.innerHTML = 'YOU LOSE!';
+        loseSound.play()
+
+    }
+    if(random === 1 && random2 === 4){
+        boxC.innerHTML = 'YOU LOSE!';
+        loseSound.play()
+
+    }
+
+
+
+    if(random === 2 && random2 === 1){
+        boxC.innerHTML = 'DRAW!';
+        drawSound.play()
+
+    }
+    if(random === 2 && random2 === 2){
+        boxC.innerHTML = 'DRAW!';
+        drawSound.play()
+
+    }
+    if(random === 2 && random2 === 3){
+        boxC.innerHTML = 'YOU WIN 逆轉勝!';
+        reverseWinSound.play()
+    }
+    if(random === 2 && random2 === 4){
+        boxC.innerHTML = 'YOU LOSE!';
+        loseSound.play()
+
+    }
+
+
+
+
+    if(random === 3 && random2 === 1){
+        boxC.innerHTML = 'YOU WIN!';
+        winSound.play()
+    }
+    if(random === 3 && random2 === 2){
+        boxC.innerHTML = 'YOU LOSE!';
+        reverseWinSound.play()
+
+    }
+    if(random === 3 && random2 === 3){
+        boxC.innerHTML = 'DRAW!';
+        drawSound.play()
+
+    }
+    if(random === 3 && random2 === 4){
+        boxC.innerHTML = 'DRAW!';
+        drawSound.play()
+
+    }
+
+
+
+
+    if(random === 4 && random2 === 1){
+        boxC.innerHTML = 'YOU WIN!';
+        winSound.play()
+    }
+    if(random === 4 && random2 === 2){
+        boxC.innerHTML = 'YOU WIN!';
+        winSound.play()
+
+    }
+    if(random === 4 && random2 === 3){
+        boxC.innerHTML = 'DRAW!';
+        drawSound.play()
+
+    }
+    if(random === 4 && random2 === 4){
+        boxC.innerHTML = 'DRAW!';
+        drawSound.play()
+
+    }
+
+
+}
+get1.onclick = getcard;
+
+
---
+++ js_jp/guide02 to game.js
@@ -0,0 +1,13 @@
+var button = document.getElementById("click1");
+var buttonSound = document.getElementById("buttonSound");
+
+button.addEventListener("click", function(event) {
+    event.preventDefault(); // 阻止链接的默认行为
+
+    buttonSound.play();
+
+    buttonSound.addEventListener("ended", function() {
+        // 音频播放结束后，执行页面导航
+        window.location.href = "poker_game_jp.html";
+    });
+});
